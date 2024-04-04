cebda6e51ec9f910364e0763142912b601128e65
Yaqoob <social.yaqoob@gmail.com>
2024-04-04 20:55:01 +0500

    change baal to plussign

--- app/src/main/java/com/orbitalsonic/bubblelevel/widget/AccelerometerDrawer.kt
+++ app/src/main/java/com/orbitalsonic/bubblelevel/widget/AccelerometerDrawer.kt
@@ -9,11 +9,15 @@ import com.orbitalsonic.bubblelevel.R
 class AccelerometerDrawer(context: Context, private val isSimple: Boolean) : ViewDrawer<PointF?> {
     private val pathPaint: Paint
     private val ballPaint: Paint
+    private val plusPaint: Paint
+
     private var path: Path? = null
     private var xPos = 0f
     private var yPos = 0f
     private val center: Point
     private var radius = 0
+    private val plusRadius: Float
+
     override fun layout(width: Int, height: Int) {
         radius = width / 8
         center[width / 2] = height / 2
@@ -21,10 +25,10 @@ class AccelerometerDrawer(context: Context, private val isSimple: Boolean) : Vie
         if (path == null) {
             val radius = width / 2f - width * 0.03f
             path = Path()
-            path!!.moveTo(center.x - radius, center.y.toFloat())
-            path!!.lineTo(center.x + radius, center.y.toFloat())
-            path!!.moveTo(center.x.toFloat(), center.y - radius)
-            path!!.lineTo(center.x.toFloat(), center.y + radius)
+            path!!.moveTo(center.x - plusRadius, center.y.toFloat())
+            path!!.lineTo(center.x + plusRadius, center.y.toFloat())
+            path!!.moveTo(center.x.toFloat(), center.y - plusRadius)
+            path!!.lineTo(center.x.toFloat(), center.y + plusRadius)
             if (!isSimple) {
                 path!!.addCircle(center.x.toFloat(), center.y.toFloat(), radius, Path.Direction.CCW)
             }
@@ -35,7 +39,18 @@ class AccelerometerDrawer(context: Context, private val isSimple: Boolean) : Vie
         //Draw grid
         canvas!!.drawPath(path!!, pathPaint)
         //Draw ball
-        canvas.drawCircle(center.x - xPos, center.y + yPos, radius.toFloat(), ballPaint)
+       // canvas.drawCircle(center.x - xPos, center.y + yPos, radius.toFloat(), ballPaint)
+
+        canvas.drawLine(
+            center.x - xPos - plusRadius, center.y + yPos,
+            center.x - xPos + plusRadius, center.y + yPos,
+            plusPaint
+        )
+        canvas.drawLine(
+            center.x - xPos, center.y + yPos - plusRadius,
+            center.x - xPos, center.y + yPos + plusRadius,
+            plusPaint
+        )
     }
 
     override fun update(value: PointF?) {
@@ -45,7 +60,7 @@ class AccelerometerDrawer(context: Context, private val isSimple: Boolean) : Vie
 
     init {
         val gridColor: Int = ContextCompat.getColor(context,R.color.dayNightColor)
-        val ballColor: Int = ContextCompat.getColor(context,R.color.dayNightColor)
+        val ballColor: Int = ContextCompat.getColor(context,R.color.white)
         pathPaint = Paint(Paint.ANTI_ALIAS_FLAG)
         pathPaint.strokeWidth = 1f
         pathPaint.style = Paint.Style.STROKE
@@ -54,5 +69,15 @@ class AccelerometerDrawer(context: Context, private val isSimple: Boolean) : Vie
         ballPaint.style = Paint.Style.FILL
         ballPaint.color = ballColor
         center = Point(0, 0)
+
+
+        plusPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            strokeWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2f, context.resources.displayMetrics)
+            style = Paint.Style.STROKE
+            color = Color.WHITE
+        }
+
+        radius = 0
+        plusRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, context.resources.displayMetrics)
     }
 }
